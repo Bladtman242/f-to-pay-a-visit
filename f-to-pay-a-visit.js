2e228d4001449f414b62ec922dcb238f8d7eb539
Bladtman <sigurt.bladt@gmail.com>
2022-09-26 14:09:56 +0200

    Waaaaaaaaay to late first commit
    includes the old eventhandler, so it will be in history

---
+++ f-to-pay-a-visit.js
@@ -0,0 +1,488 @@
+const watchMap = {
+  empty: () => ({}),
+  add: (watchMap, k) => {
+    watchMap[k] = Date.now();
+  },
+  delete: (watchMap, k) => {
+    val = watchMap[k];
+    delete watchMap[k];
+    return undefined !== val;
+  },
+  contains500ms: (watchMap, k) => {
+    const v = watchMap[k];
+    if (undefined === v) {
+      return false;
+    }
+    return v >= Date.now() - 500;
+  },
+};
+
+const DFA_ANY = {};
+const dfa = {
+  ANY: DFA_ANY,
+  empty: () => {
+    return {
+      states: {},
+    };
+  },
+  addTransition: (dfa, from, to, input, f) => {
+    const _from = dfa.states[from] || { transitions: {} };
+    const _to = dfa.states[to] || { transitions: {} };
+
+    _from.transitions[input] = {to, f};
+
+    dfa.states[from] = _from;
+    dfa.states[to] = _to;
+  },
+  setState: (dfa, state) => {
+    dfa.state = state;
+    console.log(state);
+  },
+  input: (dfa, input) => {
+    if(undefined === dfa.state){
+      throw {
+        msg:"dfa is not in a state, cannot transition",
+        dfa,
+        input,
+      };
+    }
+
+    const state = dfa.states[dfa.state]
+
+    if(undefined === state){
+      throw {
+        msg:"dfa does not have a state corresponding to its current state, cannot transition",
+        dfa,
+        input,
+      };
+    }
+
+    let transition = state.transitions[input.event.key]
+    if(transition === undefined) {
+      transition = state.transitions[DFA_ANY];
+      if(undefined !== transition){
+        console.log('following the default transition');
+      }
+    }
+
+    if(undefined === transition) {
+      // no transition for this input, do nothing
+      console.log('no transition for input', {dfa, input});
+      return;
+    }
+
+    const { to, f } = transition;
+    const stateOverride = f(input);
+    dfa.state = stateOverride ?? to;
+    console.log(dfa.state);
+  },
+}
+
+const getNAncestors = (e, n) => {
+  if (n === 0) {
+    return [];
+  } else {
+    const parent = e.parentElement;
+    if (!parent) {
+      return [];
+    } else {
+      const ancestors = getNAncestors(parent, n - 1);
+      ancestors.push(parent);
+      return ancestors;
+    }
+  }
+};
+
+const getTargetableElements = () => {
+  const isClickable =
+    (e) => e.matches('a, summary, button, input, textarea, *[role="button"], *[onclick], *[contenteditable]')
+        || window.getComputedStyle(e).cursor === 'pointer';
+
+  const screenTop = 0
+  const screenBottom = window.innerHeight;
+  const isVisible = (e) => {
+    const { top, bottom, width, height } = e.getBoundingClientRect();
+    return top < screenBottom
+        && screenTop < bottom
+        && width >= 5
+        && height >= 5;
+  }
+
+  const allElements = Array.from(document.getElementsByTagName('*'));
+  const relevantElements = allElements.filter((e) => isVisible(e) && isClickable(e));
+
+  const elementSet = new Set();
+  relevantElements.forEach((e) => {
+    const ancestors = getNAncestors(e, 5);
+    const clickableAncestor = ancestors.find((a) => elementSet.has(a))
+    if (clickableAncestor) {
+      if (!(clickableAncestor.tagName === 'A' && clickableAncestor.href)) {
+        elementSet.delete(clickableAncestor);
+        elementSet.add(e);
+      }
+    } else {
+      elementSet.add(e);
+    }
+  });
+  return [...elementSet].map((e) => ({target: e}));
+}
+
+const zIndexOfElement = (e) => window.getComputedStyle(e).zIndex;
+
+const findZIndex = (e) => {
+  const zIndexOr0 = (e) => {
+    const zi = zIndexOfElement(e);
+    return zi !== 'auto' ? parseInt(zi) : 0;
+  };
+
+  const maxParent = (e) => {
+    const zi = zIndexOr0(e);
+    return e.parentElement ? Math.max(zi, maxParent(e.parentElement)) : zi;
+  };
+
+  const childrenZIndices = [...e.querySelectorAll('*')].map(zIndexOr0);
+  return Math.max(...childrenZIndices, maxParent(e));
+};
+
+const generateTagElement = (tag) => {
+  const tagElement = document.createElement('div');
+  tagElement.className = 'tag';
+  tagElement.style.top = `${tag.top}px`;
+  tagElement.style.right =`${tag.right}px`;
+  tagElement.style.zIndex = findZIndex(tag.target);
+  tagElement.innerHTML = tag.name;
+  tagElement.target = tag.target;
+  tagElement.tagData = tag;
+  return tagElement;
+}
+
+const positionTag = (element) => {
+  const bbox = element.target.getBoundingClientRect();
+  const top = bbox.top + window.scrollY;
+  const right = window.innerWidth - (bbox.right + window.scrollX);
+  return { top, right };
+}
+
+const positionTags = (elements) => {
+  return elements.map((e) => {
+    const pos = positionTag(e);
+    return {
+      ...pos,
+      ...e,
+    }
+  });
+}
+
+const generateIdxes = (count, letters) => {
+  const width = Math.max(1, Math.ceil(Math.log2(count) / Math.log2(letters.length)));
+  const names = [];
+  stack = [[0, ""]];
+  let task = undefined;
+  while(undefined !== (task = stack.shift())) {
+    const [i, str] = task;
+    for (const l of letters) {
+      const nextStr = str + l;
+      if(i === (width - 1)) {
+        names.push(nextStr);
+      } else {
+          stack.push([i + 1, nextStr]);
+      }
+    }
+  }
+  return names
+}
+
+const nameTags = (tags) => {
+  const letters = [
+    "A", "S", "D", "F", "G",
+    "Z", "X", "C", "V", "B",
+    "Q", "W", "E", "R", "T",
+  ];
+  center = [window.innerWidth / 2, window.innerHeight / 2];
+  tags.sort((t1,t2) => {
+    const t1Rect = t1.target.getBoundingClientRect();
+    const t2Rect = t2.target.getBoundingClientRect();
+    t1DistanceToCenter = Math.sqrt(Math.abs(t1Rect.left - center[0])**2 * Math.abs(t1Rect.top = center[1])**2);
+    t2DistanceToCenter = Math.sqrt(Math.abs(t2Rect.left - center[0])**2 * Math.abs(t2Rect.top = center[1])**2);
+    return t1DistanceToCenter - t2DistanceToCenter;
+  });
+  const count = tags.length;
+  const names = generateIdxes(count, letters);
+  return tags.map((t,i) => ({ ...t, name: names[i] }));
+}
+
+let tagsInDom = [];
+let filterString = "";
+
+const css =
+`.tag {
+  --color: #b28774;
+  --background-color: #0d1321;
+  --letter-spacing: 0.2ch;
+  --horizontal-padding: 2px;
+  --vertical-padding: 2px;
+
+  position: absolute;
+  display: inline;
+
+  border: solid thin;
+  border-radius: 3px;
+  border-color: var(--color);
+  outline: solid 2px var(--background-color);
+
+  background-color: var(--background-color);
+  color: var(--color);
+
+  padding-left: calc(var(--horizontal-padding) + var(--letter-spacing));
+  padding-right: var(--horizontal-padding);
+  padding-top: var(--vertical-padding);
+  padding-bottom: var(--vertical-padding);
+
+  min-width: 2ch;
+
+  font-weight: bold;
+  font-size: 8pt;
+  font-family: monospace;
+  letter-spacing: var(--letter-spacing);
+  text-align: center;
+}
+`;
+
+// create shadow DOM
+const shadowHost = document.createElement('div');
+document.documentElement.appendChild(shadowHost);
+const shadowRoot = shadowHost.attachShadow({ mode: 'open' });
+
+// create style element
+const style = document.createElement('style');
+style.appendChild(document.createTextNode(css));
+
+// create holder element for tags
+const holder = document.createElement('section');
+
+// append style and holder elements to the shadow root
+shadowRoot.appendChild(style);
+shadowRoot.appendChild(holder);
+
+const addTagElementsToDom = (elements) => {
+  holder.append(...elements);
+}
+
+const displayTags = (tags) => {
+  addTagElementsToDom(tags);
+  tagsInDom = tags;
+}
+
+const hasModKey = (e) => {
+  return e.ctrlKey
+    || e.shiftKey
+    || e.altKey
+    || e.metaKey;
+}
+
+const isModKey = (e) => {
+  return e.getModifierState(e.key);
+}
+
+const isContentEditable = (element) => {
+  if (element === null || element === undefined || element === document) {
+    return false;
+  }
+  const ceAttribute = element.getAttribute('contenteditable');
+  const ce = ceAttribute === '' || ceAttribute === 'true';
+  const par = element.parentElement;
+
+  return ce || isContentEditable(par);
+}
+
+const inputTagNames = new Set(['INPUT', 'TEXTAREA']);
+const isInputElement = (element) => inputTagNames.has(element.tagName) || isContentEditable(element);
+
+const eventIsRelevant = (e) => {
+  return !e.defaultPrevented
+    && !isInputElement(e.target)
+    && !isModKey(e);
+}
+
+const pressedKeys = watchMap.empty();
+
+stateMachine = dfa.empty();
+//states
+const INACTIVE = "INACTIVE";
+const ACTIVE = "ACTIVE";
+const ESCAPED = "ESCAPED";
+
+const stop = (e) => {
+  e.event.stopImmediatePropagation();
+  e.event.stopPropagation();
+  e.event.preventDefault();
+};
+
+let timeoutId;
+
+dfa.addTransition(stateMachine, INACTIVE, ACTIVE, 'f', (i) => { if (!eventIsRelevant(i.event) || hasModKey(i.event)){ return INACTIVE} else { stop(i); clear(); display() } });
+dfa.addTransition(stateMachine, INACTIVE, INACTIVE, 'Escape', (i) => { document.activeElement.blur() });
+dfa.addTransition(stateMachine, INACTIVE, ESCAPED, ',', (i) => { timeoutId = setTimeout(() => {stop(i); dfa.setState(stateMachine, INACTIVE) }, 500) });
+dfa.addTransition(stateMachine, ESCAPED, INACTIVE, dfa.ANY, (i) => clearTimeout(timeoutId));
+dfa.addTransition(stateMachine, ACTIVE, ACTIVE, dfa.ANY, (i) => { stop(i); return filter(i.event) });
+dfa.addTransition(stateMachine, ACTIVE, INACTIVE, 'Escape', (i) => { stop(i); clear() });
+
+dfa.setState(stateMachine, INACTIVE);
+
+const keypressHandler = (event) => {
+  if (isModKey(event)) {
+    return;
+  }
+  dfa.input(stateMachine, { event });
+};
+
+const keypressHandler_old = (e) => {
+  // DFA: Relevance check could be input, preprocessed I'm not sure what to do
+  // with blur. It shouldn't be necessary, but clearly some wobsites prevent
+  // the regular browser handling of this?
+  //
+  // Ignore key presses we know have nothing to do with us
+  if (!eventIsRelevant(e)) {
+    if(e.key === 'Escape') {
+      document.activeElement.blur();
+    }
+    console.log('event irrelevant', e);
+    return;
+  }
+
+  const key = e.key;
+
+  //DFA: This could be handled as considering the 500ms passing an input,
+  //causing a state change, or, perhaps more conveniently, this could be done
+  //as a pre-processing step, occurring outside of the state machine
+  //
+  // check if ',' has been pressed recently, we use this as an escape, to allow
+  // users to use f and Escape to things other than using this extension
+  if (watchMap.contains500ms(pressedKeys, ',')) {
+    watchMap.delete(pressedKeys, ',');
+    console.log('event escaped');
+    return;
+  }
+
+  // escape future key press
+  if (key === ',' && !displayIsActive()) {
+    // DFA: escape/, should be handled in accordance to how escaping the the
+    // escape state after 500ms is handled (see above).
+    console.log('escape next event');
+    watchMap.add(pressedKeys, ',');
+  } else if (key === 'Escape' && displayIsActive()) {
+    // DFA: displayActive is a state (or multiple states), escape is an input
+    console.log('clear');
+    clear();
+  } else if (hasModKey(e) && !displayIsActive()) {
+    // DFA: This will be different, as we will allow modifier keys, though
+    // perhaps not on the inactive->active transition? Regardless, modifiers
+    // will be part of either the input, or processing.
+    console.log('event takes precedence', e);
+    return;
+  } else if (displayIsActive()) {
+    // DFA: displayactive will be state, the input (e here) will be input
+    console.log('filter');
+    filter(e);
+  } else if (key === 'f' && !displayIsActive()) {
+    // DFA: active is state, f is input
+    console.log('display');
+    clear();
+    display();
+  } else {
+    // DFA: This is essentially when an input comes, for which the current
+    // state has no outgoing transitions
+    console.log('event processed, without an action', e);
+    return;
+  }
+
+  // DFA: This will be part of the individual transition side effects
+  e.stopImmediatePropagation();
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+const display = () => {
+  let start = new Date();
+  const targets = getTargetableElements();
+  console.log(`found ${targets.length} targets in:`, new Date() - start);
+  start = new Date();
+  const namedTags = nameTags(targets);
+  console.log(`named tags in`, new Date() - start);
+  start = new Date();
+  const positionedTags = positionTags(namedTags);
+  console.log(`positioned tags in:`, new Date() - start);
+  start = new Date();
+  const tagElements = positionedTags.map(generateTagElement);
+  console.log(`created tag elements in:`, new Date() - start);
+  start = new Date();
+  displayTags(tagElements);
+  console.log(`added tag elements in:`, new Date() - start);
+}
+
+const clear = () => {
+  tagsInDom.forEach(e => e.remove());
+  tagsInDom = [];
+  filterString = "";
+}
+
+const displayIsActive = () => tagsInDom.length !== 0
+
+const select = (tag, keyEvent) => {
+  console.log('select', tag.target);
+  if(isInputElement(tag.target)) {
+    tag.target.focus();
+    tag.target.value = tag.target.value;
+  }
+
+  const modifiers = {
+    shiftKey: keyEvent.shiftKey,
+    ctrlKey: keyEvent.ctrlKey,
+    altKey: keyEvent.altKey,
+    metaKey: keyEvent.metaKey,
+  };
+
+  const Event = (type) => new MouseEvent(type, {
+    bubbles: true,
+    view: window,
+    ... modifiers,
+  });
+
+  ['mouseover', 'mousedown', 'mouseup', 'click'].forEach((type) =>
+    tag.target.dispatchEvent(Event(type)));
+  clear();
+}
+
+const partition = (arr, p) => {
+  const not = (f) => (x) => !f(x);
+
+  const yes = arr.filter(p);
+  const no = arr.filter(not(p));
+
+  return [yes, no];
+}
+
+const filter = (keyEvent) => {
+  const key = keyEvent.key;
+  if (key === 'Backspace') {
+    filterString = filterString.slice(0,-1);
+  } else {
+    filterString += key.toUpperCase();
+  }
+  const [matches, rest] = partition(tagsInDom, (tag) => tag.tagData.name.startsWith(filterString));
+  if (matches.length === 0) {
+    clear();
+    return INACTIVE;
+  }
+  if (matches.length === 1) {
+    select(matches[0], keyEvent);
+    return INACTIVE;
+  }
+  rest.forEach((tag) => tag.style.display = "none");
+  matches.forEach((tag) => {
+    tag.innerHTML = `<span style="opacity: 0.4">${filterString}</span>${tag.tagData.name.slice(filterString.length)}`;
+    tag.style.display = '';
+  });
+}
+
+window.addEventListener("keydown", keypressHandler, { capture: true });
